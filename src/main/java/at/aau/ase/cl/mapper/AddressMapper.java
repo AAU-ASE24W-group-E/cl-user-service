f3ca71d415aad9fe744ee3b630ea943a48d8d17f
jublebi <julianlechner31@gmail.com>
2024-11-19 14:36:29 +0100

    Created Address Mapper
    Similar to the User mapper we map between AddressEntity for database and for api.

--- src/main/java/at/aau/ase/cl/mapper/AddressMapper.java
+++ src/main/java/at/aau/ase/cl/mapper/AddressMapper.java
@@ -1,4 +1,17 @@
 package at.aau.ase.cl.mapper;
 
-public class AddressMapper {
+import at.aau.ase.cl.api.model.Address;
+import at.aau.ase.cl.api.model.AddressEntity;
+import at.aau.ase.cl.api.model.User;
+import at.aau.ase.cl.model.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AddressMapper {
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    AddressEntity map(Address address);
+
+    Address map(AddressEntity addressEntity);
 }
