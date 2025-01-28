6c642c4b800981f892bc0e9714dd33ca531248c5
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2025-01-28 21:59:23 +0100

    fix migration script

--- src/main/resources/db/migration/V1.0.1__Reset_Password_Token.sql
+++ src/main/resources/db/migration/V1.0.1__Reset_Password_Token.sql
@@ -1,7 +1,10 @@
+drop table if exists user_reset_password_tokens cascade;
+drop index if exists user_reset_password_tokens_reset_token_key;
+
 CREATE TABLE user_reset_password_tokens(
     id          UUID PRIMARY KEY NOT NULL,
     user_id     UUID             NOT NULL,
-    reset_token VARCHAR(255)     NOT NULL UNIQUE,
+    reset_token VARCHAR(255)     NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX user_reset_password_tokens_reset_token_key ON user_reset_password_tokens (reset_token);
